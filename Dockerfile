9aef2afd426c1b2ada739410c3b5887869a0f5d0
eggplants <w10776e8w@yahoo.co.jp>
2021-12-11 11:21:22 +0900

    improve

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM alpine:3.15.0
+
+ARG VERSION
+ENV VERSION ${VERSION:-master}
+RUN wget -nv "https://raw.githubusercontent.com/ContentsViewer/sh-tetris/${VERSION}/tetris" \
+    && chmod +x ./tetris \
+    && mv ./tetris /usr/local/bin/
+
+ENTRYPOINT ["tetris"]
--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,30 @@
 
 set -euo pipefail
 
+vs=(
+v1.0.0
+v1.1.0
+v1.2.0
+v2.0.0
+v2.0.1
+v2.0.2
+v2.1.0
+master
+)
+
 repo="eggplanter/sh-tetris"
 
-for i in v*/ latest/; do
-  echo "[[[ $i ]]]"
-  docker build -t "${repo}:${i/\//}" "$i"
-  docker push "${repo}:${i/\//}"
+if ! [ -f "./Dockerfile" ]; then
+  echo "err: Dockerfile is not found.">&2
+  exit 1
+fi
+
+for v in "${vs[@]}"; do
+  echo "[[[ $v ]]]"
+  docker build -t "${repo}:${v}" --build-arg VERSION="$v" .
+  [ TEST = "1" ] || docker push "${repo}:${v}"
 done
+
+echo "[[[ latest ]]]]"
+docker build -t "${repo}:latest" --build-arg VERSION="${vs[${#vs[@]}-1]}" .
+[ TEST= "1" ] || docker push "${repo}:latest"
